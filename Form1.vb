b5224bb369ded981abd0edf51254b9a3535b87e5
Eduardo Fabre Toledo <egufabre@hotmail.com>
2020-06-28 22:30:57 -0300

    ehhhh

--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Form1
+    Private Sub NToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NToolStripMenuItem.Click
 
+    End Sub
+
+    Private Sub ToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem1.Click
+
+    End Sub
 End Class
